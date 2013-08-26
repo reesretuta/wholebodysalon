0d20f0291ec4fcbf56b7eb047a4b6d93eaa0545b
Rees Retuta <rees.retuta@gmail.com>
2013-08-26 14:13:45 -0700

    remove text

--- application/views/products.php
+++ application/views/products.php
@@ -13,11 +13,4 @@
 	<h1>Products</h1><br/>
 	<p>After your hands and feet are dolled up, revamp your nail color with polishes from Mineral Fusion, Gabriel, Scotch Naturals, Zoya or Hopscotch Kids. </p>
 	<p>All of our polishes are rated “Four or Five-Free,” which means that they do not include at least four of the following toxins: formaldehyde, formaldehyde resin, dibutyl phthalate, toluene, or camphor. </p>
-	<p>Why don’t we use products with these five chemicals?</p>
-	<p>Well…</p>
-	<p>Formaldehyde is considered a human carcinogen by U.S. health agencies. Sometimes listed as “formalin,” this irritating chemical causes some to experience allergic reactions, as does its refined product, formaldehyde resin.	</p>
-	<p>Dibutyl phthalate, also known as DBP, is associated with disrupting hormone regulation, particularly in male reproductive systems. It is also a possible trigger for asthma attacks and is linked to cancer in lab animal testing. This phthalate is used to soften plastics and is commonly found in vinyl flooring, seat coverings, raincoats, shower curtains, and, most relevantly, plastic fingernails.</p>
-	<p>Like DBP, toluene is a volatile solvent that can be inhaled and absorbed through the skin and nails. Also found in gasoline, toluene can cause dizziness and temporary intoxication, and may appear on labels as “toluol” or “phenylmethane.”</p>
-	<p>Camphor is chemically manufactured from turpentine oil. It is commonly applied topically to reduce pain, irritation or swelling. It is also used on finger and toe nails to combat fungus. When applied to broken skin, camphor can quickly enter the body, concentrate and become poisonous.</p>
-	<p>So ditch those trashy toxins and get yourself some people-friendly polish!</p>
 </div>
